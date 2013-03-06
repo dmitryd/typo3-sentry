f7c7e3e5e4875b36c7a78ba62edaa0b6f8f4d885
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-03-06 22:07:22 +0400

    Set TYPO3 version requirement to 6.0+ for now.

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.2-0.0.0',
-			'typo3' => '4.5.0-6.0.99',
+			'typo3' => '6.0.0-6.0.99',
 		),
 		'conflicts' => array(
 		),
