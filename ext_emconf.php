455e86abb357a1ebb7191b64468d38f547743cf8
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-03-07 12:18:19 +0400

    Update ext_emconf with a better description. Change TYPO3 compatibility version.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Sentry',
-	'description' => 'Sentry integration with TYPO3.',
-	'category' => 'misc',
+	'title' => 'Sentry error reporting',
+	'description' => 'Sentry integration with TYPO3. This extension sends TYPO3 errors and warning to the Sentry system for monitoring and analyzing.',
+	'category' => 'services',
 	'shy' => 0,
 	'dependencies' => '',
 	'conflicts' => '',
@@ -25,19 +25,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry.dulepov@gmail.com',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.12.3',
+	'version' => '0.0.0',
 	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"6d70";s:10:"_.htaccess";s:4:"a6b1";s:20:"class.ext_update.php";s:4:"8518";s:20:"class.tx_realurl.php";s:4:"85dc";s:29:"class.tx_realurl_advanced.php";s:4:"8dd0";s:32:"class.tx_realurl_autoconfgen.php";s:4:"5226";s:28:"class.tx_realurl_tcemain.php";s:4:"fb6f";s:16:"ext_autoload.php";s:4:"a6ff";s:21:"ext_conf_template.txt";s:4:"c890";s:12:"ext_icon.gif";s:4:"ea80";s:17:"ext_localconf.php";s:4:"e297";s:14:"ext_tables.php";s:4:"7182";s:14:"ext_tables.sql";s:4:"150c";s:17:"locallang_csh.xml";s:4:"369d";s:16:"locallang_db.xml";s:4:"584a";s:14:"doc/manual.sxw";s:4:"7bfb";s:38:"modfunc1/class.tx_realurl_modfunc1.php";s:4:"0b6f";s:41:"modfunc1/class.tx_realurl_pagebrowser.php";s:4:"b616";s:22:"modfunc1/locallang.xml";s:4:"7e4f";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.2-0.0.0',
-			'typo3' => '4.5.0-6.0.99',
+			'typo3' => '4.5.0-6.1.999',
 		),
 		'conflicts' => array(
 		),
