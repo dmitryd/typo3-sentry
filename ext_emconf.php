e4a0217f23415371b20cc591281cc31303ff2fb1
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2014-02-11 19:59:25 +0400

    Enable installation on TYPO3 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.2-0.0.0',
-			'typo3' => '4.5.0-6.1.999',
+			'typo3' => '4.5.0-6.2.999',
 		),
 		'conflicts' => array(
 		),
